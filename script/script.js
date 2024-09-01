5a4e91e2c2f295d66bb0405acd8cd28fa5408ade
Rhuzzel Paramio <87302705+CodeDecoder13@users.noreply.github.com>
2024-09-01 18:22:15 +0800

    ss

--- script/script.js
+++ script/script.js
@@ -1,4 +1,7 @@
-
+document.querySelector('.toggle-btn').addEventListener('click', function() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active'); // Toggles the visibility of the nav links
+});
 
 
 
